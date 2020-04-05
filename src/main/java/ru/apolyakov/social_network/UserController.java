bc36aad9c3458d150bc979ae6cb8b2e88332b47e
Andrei Poliakov <avpgenium@yandex.ru>
2020-04-05 22:30:05 +0300

    заготовка пустого проекта

---
+++ src/main/java/ru/apolyakov/social_network/UserController.java
@@ -0,0 +1,8 @@
+package ru.apolyakov.social_network;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    
+}
---
+++ src/test/java/ru/apolyakov/social_network/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.apolyakov.social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
