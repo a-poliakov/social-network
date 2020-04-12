d4ca10e412e1cac31342724b1d6f65143d24d563
Andrei Poliakov <avpgenium@yandex.ru>
2020-04-12 11:36:03 +0300

    fix thymleaf config

--- src/main/java/ru/apolyakov/social_network/config/WebSecurityConfig.java
+++ src/main/java/ru/apolyakov/social_network/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.apolyakov.social_network.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,7 @@ import ru.apolyakov.social_network.service.UserServiceImpl;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Autowired
+    @Lazy
     private UserService userService;
 
     @Autowired
